43a4eda35725758c7f864ffae35a5d157dfd42c1
Kody Bishop <kodypbishop@gmail.com>
2020-02-11 18:24:14 -0700

    setting up file structure

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Jang0f3tt",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("../config/connection.js");
+
+let orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
+}
+
+module.exports = orm;
